ae125221baf5cfcd55fe1be8b35a73cf8268d9c1
Merchant151 <jmiller32@ggc.edu>
2020-01-18 16:42:22 -0500

    Created project Local

---
+++ LavaCave/LavaCave/Point.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LavaCave
+{
+    public class Point
+    {
+        public int[] coord;
+        public string cont;//content.
+        public Point()
+        {
+
+        }
+    }
+}
